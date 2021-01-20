1d8a46d19b79db92536e3c545e00c1738b4ccc90
Eric Lund <erl@cray.com>
2021-01-20 09:42:07 -0600

    Update files with MIT License and Copyright for export to GH

--- setup.py
+++ setup.py
@@ -1,7 +1,30 @@
+"""
+MIT License
+
+(C) Copyright [2020] Hewlett Packard Enterprise Development LP
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
 from setuptools import setup
 
 
-with open('LICENSE.txt') as license_file:
+with open('LICENSE') as license_file:
     LICENSE = license_file.read()
 
 with open('requirements.txt') as reqs_file:
@@ -12,8 +35,8 @@ with open('version') as vers_file:
 
 setup(
     name='cray',
-    author="Cray Inc.",
-    author_email="rbezdicek@cray.com",
+    author="Hewlett Packard Enterprise LP",
+    author_email="eric.lund@hpe.com",
     url="http://cray.com",
     description="Cray management and workflow tool",
     long_description="A tool to help you manage and interact with a cray",
