03d34412af25fc8aeb9064bf0ce585f3c15334d3
Forrest Jadick <forrest.jadick@gmail.com>
2021-06-24 20:17:30 -0700

    remove python install

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -26,23 +26,6 @@ VERSION="$(cat ./version)"
 
 echo $VERSION > build_version
 
-
-if command -v yum > /dev/null; then
-    yum install -y python-devel
-    yum install -y python36
-    yum install -y python36-setuptools
-    yum install -y python3-devel
-elif command -v zypper > /dev/null; then
-    zypper install -y -f -l python-pip
-    zypper install -y -f -l python-devel
-    zypper install -y -f -l python3
-    zypper install -y -f -l python3-setuptools
-    zypper install -y -f -l python3-devel
-else
-    echo "Unsupported package manager or package manager not found -- installing nothing"
-    exit 1
-fi
-
 if ! command -v pip3 > /dev/null; then
     easy_install-3.4 pip || easy_install-3.6 pip || easy_install pip
 fi
