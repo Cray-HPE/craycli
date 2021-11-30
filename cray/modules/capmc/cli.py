e4f0188d1b78c2674d8d7ec171cf49989dd316cb
Michael Jendrysik <michael.jendrysik@hpe.com>
2021-11-30 14:33:50 -0600

    Update set power cap CLI

--- cray/modules/capmc/cli.py
+++ cray/modules/capmc/cli.py
@@ -3,7 +3,7 @@ CAPMC - Cray Advanced Platform Monitoring and Control
 
 MIT License
 
-(C) Copyright [2020] Hewlett Packard Enterprise Development LP
+(C) Copyright [2020-2021] Hewlett Packard Enterprise Development LP
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
@@ -39,8 +39,7 @@ cli = generate(__file__, condense=False)
 ################################################################################
 
 SPC_NIDS_ARG = 'nids'
-SPC_NODE_ARG = 'node'
-SPC_ACCEL_ARG = 'accel'
+SPC_CTL_ARG = 'control'
 SPC_OLD_NCV = 'nids-controls-val'
 SPC_OLD_NCN = 'nids-controls-name'
 SPC_OLD_NIDS = 'nids-nid'
@@ -68,31 +67,18 @@ option('--'+SPC_NIDS_ARG, nargs=1, type=click.STRING, multiple=True,
             "may not be omitted. Ths specified NIDs must be in the ready "
             "state per the node_status command.")(CREATE_CMD)
 
-option('--'+SPC_NODE_ARG, nargs=1, type=click.INT,
-       payload_name=SPC_NODE_ARG,
-       help="Specify the desired node level power cap. The value given must "
-            "be within the range returned in the capabilities output. A value "
-            "of zero may be supplied to explicitly clear an existing node "
-            "level power cap. Nodes with high powered accelerators and high "
-            "TDP processors will be automatically power capped at the supply "
-            "limit returned per the get_power_cap_capabilities command. If a "
-            "node level power cap is specified that is within the node "
-            "control range but exceeds the supply limit, the actual power cap "
-            "assigned will be clamped at the supply limit.")(CREATE_CMD)
-
-option('--'+SPC_ACCEL_ARG, nargs=1, type=click.INT,
-       payload_name=SPC_ACCEL_ARG,
-       help="Specify the desired accelerator component power cap. The value "
-            "given must be within the range returned in the capabilities "
-            "output. A value of zero may be supplied to to explicitly clear "
-            "an accelerator power cap.  The accelerator power cap value "
-            "represents a subset of the total node level power cap. If a node "
-            "level power cap of 400 watts is applied and an accelerator power "
-            "cap of 180 watts is applied, then the total node power "
-            "consumption is limited to 400 watts. If the accelerator is "
-            "actively consuming its entire 180 watt power allocation, then "
-            "the host processor, memory subsystem, and support logic for that "
-            "node may consume a maximum of 220 watts.")(CREATE_CMD)
+option('--'+SPC_CTL_ARG, type=(click.STRING, click.INT),
+       payload_name=SPC_CTL_ARG, multiple=True,
+       help="Specify the desired power cap for the specified control. The "
+            "value given must be within the range returned in the "
+            "capabilities output. A value of zero may be supplied to "
+            "explicitly clear an existing power cap. Nodes with high powered "
+            "accelerators and high TDP processors will be automatically power "
+            "capped at the supply limit returned per the "
+            "get_power_cap_capabilities command. If a power cap is specified "
+            "that is within the control range but exceeds the supply limit, "
+            "the actual power cap assigned will be clamped at the supply "
+            "limit.")(CREATE_CMD)
 
 # Remove the generated params for the group names and group member lists.
 # Add the new target-groups option.
@@ -104,10 +90,8 @@ for p in CREATE_CMD.params:
     # only for making the UX better
     if p.payload_name == SPC_NIDS_ARG:
         params.insert(0, p)
-    elif p.payload_name == SPC_NODE_ARG:
+    elif p.payload_name == SPC_CTL_ARG:
         params.insert(1, p)
-    elif p.payload_name == SPC_ACCEL_ARG:
-        params.insert(2, p)
     else:
         params.append(p)
 
@@ -116,28 +100,20 @@ CREATE_CMD.params = params
 
 def set_power_cap_shim(func):
     """ Callback function to create our own payload """
-    def _decorator(nids, node, accel, **kwargs):
+    def _decorator(nids, control, **kwargs):
         if not nids['value']:
             raise click.BadParameter("--nids option required")
 
-        if not node and not accel:
-            raise click.BadParameter("--node and/or --accel must be supplied.")
+        if not control:
+            raise click.BadParameter("--control must be supplied.")
 
         payload = {'nids':[]}
         for n in nids['value']:
             e = {'nid': n, 'controls':[]}
-            if node:
+            for ctl in control:
                 c = {
-                    'name': 'node',
-                    'val': node
-                }
-                e['controls'].append(c)
-
-
-            if accel:
-                c = {
-                    'name': 'accel',
-                    'val': accel
+                    'name': ctl[0],
+                    'val': ctl[1]
                 }
                 e['controls'].append(c)
 
