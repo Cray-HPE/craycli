35d69b74689356b32051a7350c963bcb1d1d28bf
Ryan Bak <rbak@cray.com>
2022-04-11 08:41:41 -0700

    CASMCMS-7876: Update BOS cli for v2

--- cray/generator.py
+++ cray/generator.py
@@ -1,3 +1,26 @@
+#
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
 """ Generates CLI commands from parsed swagger file
 
 MIT License
@@ -53,9 +76,11 @@ def api(data, callback, base=''):
     """ Decorator that will send endpoint data into commands """
 
     def tags_decorator(func):  # pylint: disable=missing-docstring
-        def func_wrapper(*args, **kwargs):  # pylint: disable=missing-docstring
+        def func_wrapper(*args, data_handler=None, **kwargs):  # pylint: disable=missing-docstring
             kwargs['base'] = base
             args = _parse_data(data, **kwargs)
+            if data_handler:
+                args=data_handler(args)
             opts = args[-1]
             opts['callback'] = callback
             return func(*args[:-1], **opts)
--- tests/test_modules/test_bos.py
+++ tests/test_modules/test_bos.py
@@ -1,3 +1,26 @@
+#
+# MIT License
+#
+# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
 """ Test the bos module.
 
 MIT License
@@ -178,7 +201,6 @@ def test_cray_bos_sessiontemplate_create_full(cli_runner, rest_mock):
                                  '--enable-cfs', True,
                                  '--cfs-configuration', 'test-config',
                                  '--description', 'desc',
-                                 '--template-body', 'test-body',
                                  '--template-url', 'test-url'])
     assert result.exit_code == 0
     data = json.loads(result.output)
@@ -192,7 +214,6 @@ def test_cray_bos_sessiontemplate_create_full(cli_runner, rest_mock):
         'enable_cfs': True,
         'cfs': {'configuration': 'test-config'},
         'description': 'desc',
-        'templateBody': 'test-body',
         'templateUrl': 'test-url',
     }
     compare_dicts(expected, data['body'])
