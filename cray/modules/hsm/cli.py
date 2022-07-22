4c6082923596578bfaed39eaadc11176f5025d82
Manny Alvarez <82838734+alvarez3-hpe@users.noreply.github.com>
2022-07-22 11:38:40 -0500

    Remove HSM v1 API calls from craycli (#55)
    * Remove HSM v1 API calls from craycli
    
    CASMHMS-5628
    
    * Update copyright
    
    * Add hsm module test to increase coverage
    
    * Lowered the test coverage requirement
    
    Description:
    Lowered the test coverage requirement from 90 to 85 due to issues caused by unrelated modules with low test coverage. CASMCLOUD-1211 will handle correcting this.

--- cray/modules/hsm/cli.py
+++ cray/modules/hsm/cli.py
@@ -1,29 +1,29 @@
+# MIT License
+#
+# (C) Copyright [2020-2022] Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
 """hsm
-
-MIT License
-
-(C) Copyright [2020-2021] Hewlett Packard Enterprise Development LP
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included
-in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
 """
+
 # pylint: disable=invalid-name
-import os
 
 from cray.core import option
 from cray.generator import generate, _opt_callback
@@ -36,7 +36,8 @@ SWAGGER_OPTS = {
     }
 }
 
-HSM_API_VERSION = os.environ.get("CRAY_HSM_API_VERSION", "v2")
+# Keeping for future API versions
+# HSM_API_VERSION = os.environ.get("CRAY_HSM_API_VERSION", "v2")
 
 def setup(hsm_cli):
     """ Sets up all HSM overrides """
@@ -81,8 +82,9 @@ def setup_groups_partitions_create(hsm_cli, command):
 
     hsm_cli.commands[command].commands['create'] = create_command
 
-if HSM_API_VERSION == 'v1':
-    cli = generate(__file__, filename='swagger3_v1.json', swagger_opts=SWAGGER_OPTS)
-else:
-    cli = generate(__file__, filename='swagger3_v2.json', swagger_opts=SWAGGER_OPTS)
-    setup(cli)
+# Keeping for future API versions
+# if HSM_API_VERSION == 'v1':
+    # cli = generate(__file__, filename='swagger3_v1.json', swagger_opts=SWAGGER_OPTS)
+# else:
+cli = generate(__file__, filename='swagger3_v2.json', swagger_opts=SWAGGER_OPTS)
+setup(cli)
