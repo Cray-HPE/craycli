e63470173fcf94d291b9bb46c04998bc89727d57
Shane Unruh <87081771+shunr-hpe@users.noreply.github.com>
2022-12-19 14:06:03 -0700

    Removed key options from sls loadstate and dumpstate (#64)
    CASMHMS-5771

--- tests/test_modules/test_sls.py
+++ tests/test_modules/test_sls.py
@@ -64,7 +64,6 @@ def test_sls_dumpstate(cli_runner, rest_mock):
     outputs = [
         "cli sls dumpstate [OPTIONS] COMMAND [ARGS]...",
         "list",
-        "create",
     ]
 
     for out in outputs:
