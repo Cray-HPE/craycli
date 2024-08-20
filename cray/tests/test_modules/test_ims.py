4a50c54d9ae9c989b7df01288162c58b5a8e02d8
David Laine <laine@hpe.com>
2024-08-20 12:28:21 -0500

    CASMCMS-8979 - add status endpoint to cray ims remote-build-nodes.

--- cray/tests/test_modules/test_ims.py
+++ cray/tests/test_modules/test_ims.py
@@ -175,6 +175,7 @@ def test_cray_ims_public_keys_create_missing_required(cli_runner, rest_mock):
     assert result.exit_code == 2
     assert '--public-key' in result.output
 
+
 def test_cray_ims_remote_build_nodes_base(cli_runner, rest_mock):
     """ Test cray ims remote-build-nodes base command """
     runner, cli, _ = cli_runner
@@ -260,6 +261,39 @@ def test_cray_ims_remote_build_nodes_create_missing_required(cli_runner, rest_mo
     )
     assert result.exit_code == 2
     assert '--xname' in result.output
+
+
+def test_cray_ims_remote_build_nodes_status(cli_runner, rest_mock):
+    """ Test cray ims remote-build-nodes base command """
+    runner, cli, _ = cli_runner
+    result = runner.invoke(cli, ['ims', 'remote-build-nodes', 'status'])
+    assert result.exit_code == 0
+
+    outputs = [
+        "describe",
+        "list",
+    ]
+
+    compare_output(outputs, result.output)
+
+def test_cray_ims_remote_build_nodes_status_list(cli_runner, rest_mock):
+    """ Test cray ims public_keys list """
+    runner, cli, config = cli_runner
+    result = runner.invoke(cli, ['ims', 'remote-build-nodes', 'status', 'list'])
+    assert result.exit_code == 0
+    data = json.loads(result.output)
+    assert data['method'] == 'GET'
+    assert data['url'] == f'{config["default"]["hostname"]}/apis/ims/v3/remote-build-nodes/status'
+
+
+def test_cray_ims_remote_build_nodes_status_describe(cli_runner, rest_mock):
+    """ Test cray ims public_keys describe """
+    runner, cli, config = cli_runner
+    result = runner.invoke(cli, ['ims', 'remote-build-nodes', 'status', 'describe', 'foo'])
+    assert result.exit_code == 0
+    data = json.loads(result.output)
+    assert data['method'] == 'GET'
+    assert data['url'] == f'{config["default"]["hostname"]}/apis/ims/v3/remote-build-nodes/status/foo'
 
 
 def test_cray_ims_deleted_public_keys_base(cli_runner, rest_mock):
