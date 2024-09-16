dc669d031340dbb800135fc007695ede68b43357
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2024-09-16 15:26:45 -0400

    Update BOS Swagger

--- cray/modules/bos/cli.py
+++ cray/modules/bos/cli.py
@@ -34,12 +34,6 @@ SWAGGER_OPTS = {}
 
 cli = generate(__file__, swagger_opts=SWAGGER_OPTS)
 
-# Update v2 sessions should only be in the API -- not intended for CLI use
-del cli.commands['v2'].commands['sessions'].commands['update']
-
-# Remove all v1 endpoints
-del cli.commands['v1']
-
 # Place the v2 commands at the 'cray bos' level of the cli
 CURRENT_VERSION = 'v2'
 PRESERVE_VERSIONS = True
