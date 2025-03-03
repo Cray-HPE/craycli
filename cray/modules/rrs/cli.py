815f42b445289476118be5687f62ebb86ddb7477
Arka Pramanik <arka.pramanik@hpe.com>
2025-03-03 12:52:28 +0530

    Add License and Update swagger.yaml for critical services * Modify description of RRS Service API in swagger.yaml * Add license header to all files of RRS module

--- cray/modules/rrs/cli.py
+++ cray/modules/rrs/cli.py
@@ -1,7 +1,7 @@
 #
 #  MIT License
 #
-#  (C) Copyright 2020-2023 Hewlett Packard Enterprise Development LP
+#  (C) Copyright 2025 Hewlett Packard Enterprise Development LP
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a
 #  copy of this software and associated documentation files (the "Software"),
@@ -23,6 +23,25 @@
 #
 """ rrs """
 # pylint: disable=invalid-name
+import click
 from cray.generator import generate
 
 cli = generate(__file__)
+
+
+def _file_cb(cb):
+
+    def _cb(ctx, param, value):
+        data = value.read()
+        if cb:
+            return cb(ctx, param, data)
+        return data
+
+    return _cb
+
+
+for p in cli.commands['criticalservices'].commands['update'].params:
+    if p.payload_name == 'new-services':
+        p.type = click.File(mode='r')
+        p.callback = _file_cb(p.callback)
+        break
